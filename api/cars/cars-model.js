21864395a39053873ebc3c38012c6479dec1d2c5
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-24 12:45:49 -0800

    updated cars-model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -11,16 +11,19 @@ function getById() {
 async function add(car) {
   return db('cars').insert(car)
     .then(([id]) => {
-      return getById(id)
+      return db('cars').where('id', id).first();
     });
 };
 
 async function update(id, updates) {
-  return db('cars').where('id', id).update(updates);
+  return db('cars').where('id', id).update(updates)
+    .then(() => {
+      return getById(id);
+    });
 };
 
 function remove(id) {
-  return null;
+  return db('cars').where('id', id).del();
 };
 
 module.exports = {
