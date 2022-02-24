c8386e897d7d30593ea55107fd0613a27741134f
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-24 12:37:04 -0800

    migration updated

--- data/migrations/20220224195542_cars.js
+++ data/migrations/20220224195542_cars.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('cars', tbl => {
+      tbl.increments();
+      tbl.string('make', 128).notNullable();
+      tbl.string('model', 128).notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('cars');
 };
