a666835f0d63baa99c46255c958d23ea1f48f7b3
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-24 12:59:09 -0800

    updated router and middleware files

---
+++ api/cars/cars-middleware.js
@@ -0,0 +1,30 @@
+const Cars = require('./cars-model');
+
+exports.checkId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const car = await Cars.getById(id);
+    if(!car) {
+      next({ status: 404, message: `car with id of ${id} not found` })
+    } else {
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
+
+exports.checkPayload = async (req, res, next) => {
+  const { make, model } = req.body;
+  try {
+    if(make === null || make === undefined || make.trim() === '') {
+      next({ status: 400, message: 'car make is required' })
+    } else if (model === null || model === undefined || model.trim() === '') {
+      next({ status: 400, message: 'car model is required' })
+    } else {
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,20 +1,40 @@
 const router = require('express').Router();
 const Cars = require('./cars-model');
+const { checkId, checkPayload } = require('./cars-middleware');
 
-router.get('/', (req, res) => {
-
+router.get('/', (req, res, next) => {
+  Cars.getAll()
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(next);
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', checkId, (req, res, next) => {
+  const { id } = req.params;
+  Cars.getById(id)
+    .then(car => {
+      res.json(car);
+    })
+    .catch(next);
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', checkPayload, async (req, res, next) => {
+  try {
+    const newCar = await Cars.add({ make: req.make.trim(), model: req.model.trim() });
+    res.status(201).json(newCar);
+  } catch(err) {
+    next(err);
+  };
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', checkId, (req, res, next) => {
+  const { id } = req.params;
+  Cars.remove(id)
+    .then(delCar => {
+      res.json(delCar)
+    })
+    .catch(next);
 });
 
 module.exports = router;
